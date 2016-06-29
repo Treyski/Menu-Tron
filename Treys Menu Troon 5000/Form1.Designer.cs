cde486c5435cea6d4fcc222ffdb8d39fcc0444e4
Brett Balzen <Brett Balzen>
2016-06-29 12:01:16 -0600

    Name Change

--- Treys Menu Troon 5000/Form1.Designer.cs
+++ Treys Menu Troon 5000/Form1.Designer.cs
@@ -42,20 +42,22 @@
             this.txbTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txbTitle.Enabled = false;
             this.txbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Bold);
-            this.txbTitle.Location = new System.Drawing.Point(86, 43);
+            this.txbTitle.Location = new System.Drawing.Point(115, 53);
+            this.txbTitle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txbTitle.Name = "txbTitle";
-            this.txbTitle.Size = new System.Drawing.Size(340, 41);
+            this.txbTitle.Size = new System.Drawing.Size(453, 49);
             this.txbTitle.TabIndex = 0;
-            this.txbTitle.Text = "Me-menu-ory";
+            this.txbTitle.Text = "What\' Good?";
             this.txbTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txbTitle.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
             // btnDQ
             // 
             this.btnDQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.btnDQ.Location = new System.Drawing.Point(25, 116);
+            this.btnDQ.Location = new System.Drawing.Point(33, 143);
+            this.btnDQ.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDQ.Name = "btnDQ";
-            this.btnDQ.Size = new System.Drawing.Size(213, 59);
+            this.btnDQ.Size = new System.Drawing.Size(284, 73);
             this.btnDQ.TabIndex = 1;
             this.btnDQ.Text = "Dairy Queen";
             this.btnDQ.UseVisualStyleBackColor = true;
@@ -64,9 +66,10 @@
             // btnRedRobins
             // 
             this.btnRedRobins.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.btnRedRobins.Location = new System.Drawing.Point(272, 238);
+            this.btnRedRobins.Location = new System.Drawing.Point(363, 293);
+            this.btnRedRobins.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRedRobins.Name = "btnRedRobins";
-            this.btnRedRobins.Size = new System.Drawing.Size(211, 61);
+            this.btnRedRobins.Size = new System.Drawing.Size(281, 75);
             this.btnRedRobins.TabIndex = 2;
             this.btnRedRobins.Text = "Red Robins";
             this.btnRedRobins.UseVisualStyleBackColor = true;
@@ -75,9 +78,10 @@
             // btn54th
             // 
             this.btn54th.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.btn54th.Location = new System.Drawing.Point(25, 238);
+            this.btn54th.Location = new System.Drawing.Point(33, 293);
+            this.btn54th.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn54th.Name = "btn54th";
-            this.btn54th.Size = new System.Drawing.Size(213, 60);
+            this.btn54th.Size = new System.Drawing.Size(284, 74);
             this.btn54th.TabIndex = 3;
             this.btn54th.Text = "54th Street";
             this.btn54th.UseVisualStyleBackColor = true;
@@ -86,9 +90,10 @@
             // btnStar
             // 
             this.btnStar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.btnStar.Location = new System.Drawing.Point(270, 116);
+            this.btnStar.Location = new System.Drawing.Point(360, 143);
+            this.btnStar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStar.Name = "btnStar";
-            this.btnStar.Size = new System.Drawing.Size(213, 59);
+            this.btnStar.Size = new System.Drawing.Size(284, 73);
             this.btnStar.TabIndex = 4;
             this.btnStar.Text = "Starbucks";
             this.btnStar.UseVisualStyleBackColor = true;
@@ -96,18 +101,19 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lime;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(503, 376);
+            this.ClientSize = new System.Drawing.Size(671, 463);
             this.Controls.Add(this.btnStar);
             this.Controls.Add(this.btn54th);
             this.Controls.Add(this.btnRedRobins);
             this.Controls.Add(this.btnDQ);
             this.Controls.Add(this.txbTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "What\'s Good?";
             this.ResumeLayout(false);
             this.PerformLayout();
 
