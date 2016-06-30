04ec21d22c134a1aa7dbefbb229a90a5dcb719a8
Trey Balzen <t3balzen@gmail.com>
2016-06-30 10:46:32 -0500

    Color

--- Treys Menu Troon 5000/FiftyFourthMainMenu.Designer.cs
+++ Treys Menu Troon 5000/FiftyFourthMainMenu.Designer.cs
@@ -45,134 +45,147 @@
             // 
             // btnMunch
             // 
+            this.btnMunch.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnMunch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btnMunch.ForeColor = System.Drawing.SystemColors.ControlText;
             this.btnMunch.Location = new System.Drawing.Point(12, 25);
             this.btnMunch.Name = "btnMunch";
             this.btnMunch.Size = new System.Drawing.Size(175, 60);
             this.btnMunch.TabIndex = 0;
             this.btnMunch.Text = "Munchies";
-            this.btnMunch.UseVisualStyleBackColor = true;
+            this.btnMunch.UseVisualStyleBackColor = false;
             this.btnMunch.Click += new System.EventHandler(this.btnMunch_Click);
             // 
             // btnSoup
             // 
+            this.btnSoup.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnSoup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btnSoup.Location = new System.Drawing.Point(441, 25);
             this.btnSoup.Name = "btnSoup";
             this.btnSoup.Size = new System.Drawing.Size(175, 60);
             this.btnSoup.TabIndex = 1;
             this.btnSoup.Text = "Soup";
-            this.btnSoup.UseVisualStyleBackColor = true;
+            this.btnSoup.UseVisualStyleBackColor = false;
             this.btnSoup.Click += new System.EventHandler(this.btnSoup_Click);
             // 
             // btnSal
             // 
+            this.btnSal.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnSal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btnSal.Location = new System.Drawing.Point(224, 25);
             this.btnSal.Name = "btnSal";
             this.btnSal.Size = new System.Drawing.Size(175, 60);
             this.btnSal.TabIndex = 2;
             this.btnSal.Text = "Salads";
-            this.btnSal.UseVisualStyleBackColor = true;
+            this.btnSal.UseVisualStyleBackColor = false;
             this.btnSal.Click += new System.EventHandler(this.btnSal_Click);
             // 
             // btnMileBurg
             // 
+            this.btnMileBurg.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnMileBurg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btnMileBurg.Location = new System.Drawing.Point(224, 204);
             this.btnMileBurg.Name = "btnMileBurg";
             this.btnMileBurg.Size = new System.Drawing.Size(175, 60);
             this.btnMileBurg.TabIndex = 3;
             this.btnMileBurg.Text = "Mile High Burgers";
-            this.btnMileBurg.UseVisualStyleBackColor = true;
+            this.btnMileBurg.UseVisualStyleBackColor = false;
             this.btnMileBurg.Click += new System.EventHandler(this.btnMileBurg_Click);
             // 
             // btnCom
             // 
+            this.btnCom.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnCom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btnCom.Location = new System.Drawing.Point(441, 113);
             this.btnCom.Name = "btnCom";
             this.btnCom.Size = new System.Drawing.Size(175, 60);
             this.btnCom.TabIndex = 4;
             this.btnCom.Text = "Combos";
-            this.btnCom.UseVisualStyleBackColor = true;
+            this.btnCom.UseVisualStyleBackColor = false;
             this.btnCom.Click += new System.EventHandler(this.btnCom_Click);
             // 
             // btnSuperS
             // 
+            this.btnSuperS.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnSuperS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btnSuperS.Location = new System.Drawing.Point(224, 292);
             this.btnSuperS.Name = "btnSuperS";
             this.btnSuperS.Size = new System.Drawing.Size(175, 60);
             this.btnSuperS.TabIndex = 5;
             this.btnSuperS.Text = "Super Sandwiches";
-            this.btnSuperS.UseVisualStyleBackColor = true;
+            this.btnSuperS.UseVisualStyleBackColor = false;
             this.btnSuperS.Click += new System.EventHandler(this.btnSuperS_Click);
             // 
             // btnCfood
             // 
+            this.btnCfood.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnCfood.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btnCfood.Location = new System.Drawing.Point(224, 113);
             this.btnCfood.Name = "btnCfood";
             this.btnCfood.Size = new System.Drawing.Size(175, 60);
             this.btnCfood.TabIndex = 6;
             this.btnCfood.Text = "Seafood";
-            this.btnCfood.UseVisualStyleBackColor = true;
+            this.btnCfood.UseVisualStyleBackColor = false;
             this.btnCfood.Click += new System.EventHandler(this.btnCfood_Click);
             // 
             // btnPasta
             // 
+            this.btnPasta.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnPasta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btnPasta.Location = new System.Drawing.Point(12, 113);
             this.btnPasta.Name = "btnPasta";
             this.btnPasta.Size = new System.Drawing.Size(175, 60);
             this.btnPasta.TabIndex = 7;
             this.btnPasta.Text = "Pasta";
-            this.btnPasta.UseVisualStyleBackColor = true;
+            this.btnPasta.UseVisualStyleBackColor = false;
             this.btnPasta.Click += new System.EventHandler(this.btnPasta_Click);
             // 
             // btnDeepSfav
             // 
+            this.btnDeepSfav.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnDeepSfav.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btnDeepSfav.Location = new System.Drawing.Point(441, 204);
             this.btnDeepSfav.Name = "btnDeepSfav";
             this.btnDeepSfav.Size = new System.Drawing.Size(175, 60);
             this.btnDeepSfav.TabIndex = 8;
             this.btnDeepSfav.Text = "Deep South Favorites";
-            this.btnDeepSfav.UseVisualStyleBackColor = true;
+            this.btnDeepSfav.UseVisualStyleBackColor = false;
             this.btnDeepSfav.Click += new System.EventHandler(this.btnDeepSfav_Click);
             // 
             // btn54Fav
             // 
+            this.btn54Fav.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btn54Fav.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btn54Fav.Location = new System.Drawing.Point(12, 204);
             this.btn54Fav.Name = "btn54Fav";
             this.btn54Fav.Size = new System.Drawing.Size(175, 60);
             this.btn54Fav.TabIndex = 9;
             this.btn54Fav.Text = "Five Four Favorites";
-            this.btn54Fav.UseVisualStyleBackColor = true;
+            this.btn54Fav.UseVisualStyleBackColor = false;
             this.btn54Fav.Click += new System.EventHandler(this.btn54Fav_Click);
             // 
             // btnGringoMEX
             // 
+            this.btnGringoMEX.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnGringoMEX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btnGringoMEX.Location = new System.Drawing.Point(12, 292);
             this.btnGringoMEX.Name = "btnGringoMEX";
             this.btnGringoMEX.Size = new System.Drawing.Size(175, 60);
             this.btnGringoMEX.TabIndex = 10;
             this.btnGringoMEX.Text = "Gringo\'s Mexican Kitchen";
-            this.btnGringoMEX.UseVisualStyleBackColor = true;
+            this.btnGringoMEX.UseVisualStyleBackColor = false;
             this.btnGringoMEX.Click += new System.EventHandler(this.btnGringoMEX_Click);
             // 
             // btnStea
             // 
+            this.btnStea.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnStea.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.btnStea.Location = new System.Drawing.Point(441, 292);
             this.btnStea.Name = "btnStea";
             this.btnStea.Size = new System.Drawing.Size(175, 60);
             this.btnStea.TabIndex = 11;
             this.btnStea.Text = "The Streets Steakhouse";
-            this.btnStea.UseVisualStyleBackColor = true;
+            this.btnStea.UseVisualStyleBackColor = false;
             this.btnStea.Click += new System.EventHandler(this.btnStea_Click);
             // 
             // FiftyFourthMainMenu
--- Treys Menu Troon 5000/Form1.Designer.cs
+++ Treys Menu Troon 5000/Form1.Designer.cs
@@ -42,10 +42,9 @@
             this.txbTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txbTitle.Enabled = false;
             this.txbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Bold);
-            this.txbTitle.Location = new System.Drawing.Point(115, 53);
-            this.txbTitle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbTitle.Location = new System.Drawing.Point(86, 43);
             this.txbTitle.Name = "txbTitle";
-            this.txbTitle.Size = new System.Drawing.Size(453, 49);
+            this.txbTitle.Size = new System.Drawing.Size(340, 41);
             this.txbTitle.TabIndex = 0;
             this.txbTitle.Text = "What\' Good?";
             this.txbTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -53,65 +52,64 @@
             // 
             // btnDQ
             // 
+            this.btnDQ.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnDQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.btnDQ.Location = new System.Drawing.Point(33, 143);
-            this.btnDQ.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDQ.Location = new System.Drawing.Point(25, 116);
             this.btnDQ.Name = "btnDQ";
-            this.btnDQ.Size = new System.Drawing.Size(284, 73);
+            this.btnDQ.Size = new System.Drawing.Size(213, 59);
             this.btnDQ.TabIndex = 1;
             this.btnDQ.Text = "Dairy Queen";
-            this.btnDQ.UseVisualStyleBackColor = true;
+            this.btnDQ.UseVisualStyleBackColor = false;
             this.btnDQ.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnRedRobins
             // 
+            this.btnRedRobins.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnRedRobins.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.btnRedRobins.Location = new System.Drawing.Point(363, 293);
-            this.btnRedRobins.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRedRobins.Location = new System.Drawing.Point(272, 238);
             this.btnRedRobins.Name = "btnRedRobins";
-            this.btnRedRobins.Size = new System.Drawing.Size(281, 75);
+            this.btnRedRobins.Size = new System.Drawing.Size(211, 61);
             this.btnRedRobins.TabIndex = 2;
             this.btnRedRobins.Text = "Red Robins";
-            this.btnRedRobins.UseVisualStyleBackColor = true;
+            this.btnRedRobins.UseVisualStyleBackColor = false;
             this.btnRedRobins.Click += new System.EventHandler(this.btnRedRobins_Click);
             // 
             // btn54th
             // 
+            this.btn54th.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btn54th.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.btn54th.Location = new System.Drawing.Point(33, 293);
-            this.btn54th.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn54th.Location = new System.Drawing.Point(25, 238);
             this.btn54th.Name = "btn54th";
-            this.btn54th.Size = new System.Drawing.Size(284, 74);
+            this.btn54th.Size = new System.Drawing.Size(213, 60);
             this.btn54th.TabIndex = 3;
             this.btn54th.Text = "54th Street";
-            this.btn54th.UseVisualStyleBackColor = true;
+            this.btn54th.UseVisualStyleBackColor = false;
             this.btn54th.Click += new System.EventHandler(this.btn54th_Click);
             // 
             // btnStar
             // 
+            this.btnStar.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnStar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.btnStar.Location = new System.Drawing.Point(360, 143);
-            this.btnStar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStar.Location = new System.Drawing.Point(270, 116);
             this.btnStar.Name = "btnStar";
-            this.btnStar.Size = new System.Drawing.Size(284, 73);
+            this.btnStar.Size = new System.Drawing.Size(213, 59);
             this.btnStar.TabIndex = 4;
             this.btnStar.Text = "Starbucks";
-            this.btnStar.UseVisualStyleBackColor = true;
+            this.btnStar.UseVisualStyleBackColor = false;
             this.btnStar.Click += new System.EventHandler(this.btnStar_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lime;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(671, 463);
+            this.ClientSize = new System.Drawing.Size(503, 376);
             this.Controls.Add(this.btnStar);
             this.Controls.Add(this.btn54th);
             this.Controls.Add(this.btnRedRobins);
             this.Controls.Add(this.btnDQ);
             this.Controls.Add(this.txbTitle);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "What\'s Good?";
             this.ResumeLayout(false);
